27789aff4ef5ddff73b3711c0bc67594056373b7
jedijoe93 <jedijoe93@gmail.com>
2020-12-06 23:37:17 -0500

    1.0.1, now with thermometer content

--- src/main/java/com/jedijoe/coldsnaphorde/Events/ItemEvents.java
+++ src/main/java/com/jedijoe/coldsnaphorde/Events/ItemEvents.java
@@ -14,16 +14,16 @@ public class ItemEvents {
     public static void CheckThermometer(PlayerInteractEvent.RightClickItem event){
         if(event.getItemStack().getItem().equals(Register.THERMOMETER.get())){
             PlayerEntity player = event.getPlayer();
+            float temp = player.world.getBiomeManager().getBiome(player.getPosition()).getTemperature(player.getPosition());
+            String code = "MISSING";
+            if (temp < 0.3){code = "Cold";}
+            else if(temp >= 0.3 && temp < 0.9){code = "Neutral";}
+            else if(temp >= 0.9 && temp < 1.5){code = "Warm";}
+            else if(temp >= 1.5){code = "Hot";}
             if(ColdSnapHorde.sconfig.TECHNICALTHERMOMETER.get()){
-                player.sendStatusMessage(new StringTextComponent("Temperature: " + Float.toString(player.world.getBiomeManager().getBiome(player.getPosition()).getTemperature(player.getPosition()))), true);
+                player.sendStatusMessage(new StringTextComponent("Temperature: " + Float.toString(temp) + " (" + code + ")"), true);
             }
             else{
-                float temp = player.world.getBiomeManager().getBiome(player.getPosition()).getTemperature(player.getPosition());
-                String code = "MISSING";
-                if (temp < 0.3){code = "Cold";}
-                else if(temp >= 0.3 && temp < 0.9){code = "Neutral";}
-                else if(temp >= 0.9 && temp < 1.5){code = "Warm";}
-                else if(temp >= 1.5){code = "Hot";}
                 player.sendStatusMessage(new StringTextComponent("Temperature: " + code), true);
 
             }
